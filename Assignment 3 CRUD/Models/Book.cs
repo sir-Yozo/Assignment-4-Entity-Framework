33f0257af4e8a20e5c5b1d4a147cb01945873b7e
sir-Yozo <donlavapie@gmail.com>
2025-03-20 17:12:39 -0600

    Add project files.

---
+++ Assignment 3 CRUD/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_2_Controller___Model.Models
+{
+    public class Book
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Author { get; set; }
+
+        [Required]
+        public DateTime PublishedDate { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Genre { get; set; }
+
+        [Required]
+        public bool Availability { get; set; }
+    }
+}
