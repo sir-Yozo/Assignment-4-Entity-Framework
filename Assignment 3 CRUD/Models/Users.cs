12c7c095e2d565b0b797418d5127aa6b960b4ff1
l-lavapie071 <l.lavapie071@mybvc.ca>
2025-03-24 18:24:28 -0600

    Start Login

--- Assignment 3 CRUD/Models/Users.cs
+++ Assignment 3 CRUD/Models/Users.cs
@@ -1,11 +1,9 @@
-﻿
-namespace Assignment_3_CRUD___Model.Models
+﻿namespace Assignment_3_CRUD___Model.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; } 
     }
- 
 }
